1cd1eb4b11ecaa3b295b504b9b9a3297ec3a1c6e
Janey Slinger <janeyslinger@gmail.com>
2022-09-19 15:24:59 +0100

    generate game board

--- Match Three/Scripts/TileGrid.gd
+++ Match Three/Scripts/TileGrid.gd
@@ -1,27 +1,52 @@
+tool
 extends Node2D
+
+export(int) var board_size setget set_board_size
+
 var click_number
 var rows_clicked = {"first": null, "second": null}
 var columns_clicked = {"first": null, "second": null}
 var tiles_clicked = {"first": null, "second": null}
 
+var game_tile = preload("res://Scenes/GameTile.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("in ready")
 	click_number = 1
+	generate_board()
 	arrange_tiles()
+	
+func set_board_size(new_board_size):
+	print("in set_board_size")
+	board_size = new_board_size
+	
+func generate_board():
+	var colours = ["pink", "green", "teal", "orange"]
+	for column_index in board_size:
+		for row_index in board_size:
+			randomize()
+			var tile_instance = game_tile.instance()
+			tile_instance.colour = colours[randi() % colours.size()]
+			tile_instance.row = row_index
+			tile_instance.column = column_index
+			add_child(tile_instance)
+		
 
 func click_event(row, column, tile_ref):
-	if(click_number == 1):
-		rows_clicked["first"] = row
-		columns_clicked["first"] = column
-		tiles_clicked["first"] = tile_ref
-		click_number = 2
-	elif(click_number == 2):
-		rows_clicked["second"] = row
-		columns_clicked["second"] = column
-		tiles_clicked["second"] = tile_ref
-		if(are_tiles_perpendicular()):
-			swap_tiles()
-		click_number = 1
+	match click_number:
+		1:
+			rows_clicked["first"] = row
+			columns_clicked["first"] = column
+			tiles_clicked["first"] = tile_ref
+			click_number = 2
+		2:
+			rows_clicked["second"] = row
+			columns_clicked["second"] = column
+			tiles_clicked["second"] = tile_ref
+			if(are_tiles_perpendicular()):
+				swap_tiles()
+			click_number = 1
 
 func are_tiles_perpendicular():
 	return rows_clicked["first"] == rows_clicked["second"] or columns_clicked["first"] == columns_clicked["second"]
@@ -44,9 +69,9 @@ func swap_tiles():
 	tiles_clicked = {"first": null, "second": null}
 	temp_tile_info = null
 	
-	
 
 func arrange_tiles():
+	print("arranging tiles")
 	for child in get_children():
 		var row_position = 32 * child.row + 16
 		var column_position = 32 * child.column + 16
@@ -58,43 +83,43 @@ func arrange_tiles():
 	
 
 func is_match(tile):
-	var texture_to_match = tile.texture
+	var colour_to_match = tile.colour
 
 	var tile_to_left = get_tile_at_position(tile.row, tile.column - 1)
 	var tile_to_right = get_tile_at_position(tile.row, tile.column + 1)
-	if(do_textures_match(texture_to_match, tile_to_left, tile_to_right)):
+	if(do_colours_match(colour_to_match, tile_to_left, tile_to_right)):
 		print("matched on both sides horizontally")
 		remove_matched_tiles([tile, tile_to_left, tile_to_right])
 		return true
 
 	var tile_two_to_the_right = get_tile_at_position(tile.row, tile.column + 2)
-	if(do_textures_match(texture_to_match, tile_to_right, tile_two_to_the_right)):
+	if(do_colours_match(colour_to_match, tile_to_right, tile_two_to_the_right)):
 		print("matched two to the right")
 		remove_matched_tiles([tile, tile_to_right, tile_two_to_the_right])
 		return true
 		
 	#check two to the left
 	var tile_two_to_the_left = get_tile_at_position(tile.row, tile.column - 2)
-	if(do_textures_match(texture_to_match, tile_to_left, tile_two_to_the_left)):
+	if(do_colours_match(colour_to_match, tile_to_left, tile_two_to_the_left)):
 		print("matched two to the left")
 		remove_matched_tiles([tile, tile_to_left, tile_two_to_the_left])
 		return true
 		
 	var tile_to_top = get_tile_at_position(tile.row - 1, tile.column)
 	var tile_to_bottom = get_tile_at_position(tile.row + 1, tile.column)
-	if(do_textures_match(texture_to_match, tile_to_top, tile_to_bottom)):
+	if(do_colours_match(colour_to_match, tile_to_top, tile_to_bottom)):
 		print("matched both sides vertically")
 		remove_matched_tiles([tile, tile_to_top, tile_to_bottom])
 		return true
 	#check two to the top
 	var tile_two_to_top = get_tile_at_position(tile.row - 2, tile.column)
-	if(do_textures_match(texture_to_match, tile_to_top, tile_two_to_top)):
+	if(do_colours_match(colour_to_match, tile_to_top, tile_two_to_top)):
 		print("matched two to the top")
 		remove_matched_tiles([tile, tile_to_top, tile_two_to_top])
 		return true
 		
 	var tile_two_to_bottom = get_tile_at_position(tile.row + 2, tile.column)
-	if(do_textures_match(texture_to_match, tile_to_bottom, tile_two_to_bottom)):
+	if(do_colours_match(colour_to_match, tile_to_bottom, tile_two_to_bottom)):
 		print("matched two to the bottom")
 		remove_matched_tiles([tile, tile_to_bottom, tile_two_to_bottom])
 		return true
@@ -104,9 +129,9 @@ func get_tile_at_position(row, column):
 		if child.row == row and child.column == column:
 			return child
 
-func do_textures_match(texture, tile_1, tile_2):
+func do_colours_match(colour, tile_1, tile_2):
 	if(tile_1 != null and tile_2 != null):
-		if(tile_1.texture == texture and tile_2.texture == texture):
+		if(tile_1.colour == colour and tile_2.colour == colour):
 			return true
 
 func remove_matched_tiles(tiles):
